1f94d87fc684c2e6e53eaafb32ab9a2af46d422c
Drue <elcdrue@users.noreply.github.com>
2026-07-26 14:00:09 +0700

    Update example.go
    Extended checks and options

--- example.go
+++ example.go
@@ -1,9 +1,11 @@
-package mbserver
+// mb1984 project main.go
+package main
 
 import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"strconv"
 	"time"
 
@@ -14,6 +16,15 @@ import (
 var iLowerID, iUpperID, iTcpPort, iBaudRate, iDataBits, iStopBits, iParity, iTimeOut int
 var ofs1, ofs2 int
 var sComPort, sIp, sFunction string
+
+func isValidIP(sIp string) {
+	ip := net.ParseIP(sIp)
+	return ip != nil
+}
+
+func listComPorts() {
+
+}
 
 func main() {
 	flag.StringVar(&sIp, "ip", "0.0.0.0", "Listen on IP")
@@ -33,6 +44,23 @@ func main() {
 	var lowerID byte = byte(iLowerID)
 	var upperID byte = byte(iUpperID)
 
+	if lowerID == 0 {
+		log.Fatalln("Incorrect lower Unit ID!")
+	}
+
+	if upperID < lowerID || upperID == 0 {
+		log.Fatalln("Incorrent upper Unit ID!")
+		upperID = lowerID
+	}
+
+	if iTcpPort < 1 || iTcpPort > 65535 {
+		log.Fatalln("Incorrect TCP port!")
+	}
+
+	if !isValidIP(sIp) {
+		log.Fatalln("Incorrent IP address!")
+	}
+
 	serv := mbserver.NewServer(lowerID, upperID, uint16(ofs1), uint16(ofs2))
 	tcpPort := strconv.Itoa(iTcpPort)
 
@@ -41,6 +69,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		if len(ports) == 0 {
 			log.Fatal("No serial ports found!")
 		}
@@ -48,6 +77,7 @@ func main() {
 		for _, port := range ports {
 			fmt.Printf("Found port: %v\n", port)
 		}
+
 		return
 	} else if sComPort != "" {
 		log.Println("use com port with name", sComPort)
@@ -64,6 +94,35 @@ func main() {
 		if err != nil {
 			log.Fatalf("failed to listen, got %v\n", err)
 		}
+	}
+
+	log.Printf("===================================\n")
+	if iLowerID < iUpperID {
+		log.Printf("Server will respond to requests with SlaveID from %d to %d\n", iLowerID, iUpperID)
+	} else {
+		log.Printf("Server will respond to requests with SlaveID %d\n", iLowerID)
+	}
+
+	log.Printf("===================================\n")
+	log.Printf("TCP Server listen on ip: %s port: %d\n", sIp, iTcpPort)
+	log.Printf("===================================\n")
+	if sComPort != "" {
+		log.Printf("COM port parameters:\n")
+		log.Printf("Name: %s\n", sComPort)
+		log.Printf("Speed: %d\n", iBaudRate)
+		log.Printf("Data bits: %d\n", iDataBits)
+		log.Printf("Stop bits: %d\n", iStopBits)
+		if iParity == 0 {
+			log.Println("Parity: None")
+		}
+		if iParity == 1 {
+			log.Println("Parity: Odd")
+		}
+		if iParity == 2 {
+			log.Println("Parity: Even")
+		}
+	} else {
+		log.Printf("COM port is not listen!\n")
 	}
 
 	err := serv.ListenTCP("0.0.0.0:" + tcpPort)
@@ -102,6 +161,8 @@ func setStopBits(stopBits int) (serialStopBits serial.StopBits) {
 		serialStopBits = serial.TwoStopBits
 	case 15:
 		serialStopBits = serial.OnePointFiveStopBits
+	default:
+		serialStopBits = serial.OneStopBit
 	}
 	return
 }
